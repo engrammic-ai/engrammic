43f27832e96e3f838836a631d003ac552770b7f8
NovusEdge <novusedge0@gmail.com>
2026-05-04 17:22:53 +0300

    feat(benchmarks): finalize custodian stress harness

--- benchmarks/custodian_stress/__init__.py
+++ benchmarks/custodian_stress/__init__.py
@@ -1,5 +1,11 @@
 """Custodian stress testing harness."""
 
+from benchmarks.custodian_stress.harness import HarnessConfig, HarnessResult, StressHarness
 from benchmarks.custodian_stress.scenarios.base import ScenarioResult
 
-__all__ = ["ScenarioResult"]
+__all__ = [
+    "HarnessConfig",
+    "HarnessResult",
+    "ScenarioResult",
+    "StressHarness",
+]
