a922f540224748b96cf96d85cfa2f1c8ef427218
NovusEdge <novusedge0@gmail.com>
2026-06-21 04:24:27 +0300

    feat(sage): add sage_decayer_job with hourly schedule (Phase D)

--- src/context_service/pipelines/jobs/__init__.py
+++ src/context_service/pipelines/jobs/__init__.py
@@ -1,6 +1,7 @@
 """Dagster job definitions for context-service."""
 
 from context_service.pipelines.jobs.beacon_sender import beacon_sender_job
+from context_service.pipelines.jobs.decayer_job import sage_decayer_job, sage_decayer_schedule
 from context_service.pipelines.jobs.groundskeeper_job import groundskeeper_nightly
 from context_service.pipelines.jobs.legacy_embed_migration_job import (
     sage_legacy_embed_migration_job,
@@ -16,15 +17,16 @@ from context_service.pipelines.jobs.synthesizer_job import (
     sage_synthesizer_job,
     sage_synthesizer_schedule,
 )
+from context_service.pipelines.jobs.detector_job import sage_detector_job
 from context_service.pipelines.jobs.usage_retention import (
     usage_retention_job,
     usage_retention_schedule,
 )
-from context_service.pipelines.jobs.validator_job import sage_validator_job
-
 __all__ = [
     "beacon_sender_job",
     "groundskeeper_nightly",
+    "sage_decayer_job",
+    "sage_decayer_schedule",
     "orphan_chain_recovery_job",
     "orphan_recovery_schedule",
     "reembed_migration",
@@ -33,7 +35,7 @@ __all__ = [
     "sage_promoter_schedule",
     "sage_synthesizer_job",
     "sage_synthesizer_schedule",
-    "sage_validator_job",
+    "sage_detector_job",
     "spo_backfill_job",
     "usage_retention_job",
     "usage_retention_schedule",
